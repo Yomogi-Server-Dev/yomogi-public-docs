2cadcfbbb71aea5af8bf5086e11a324ce6a65f68
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-10-17 22:06:21 +0900

    fix organizationName,projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yomogi-server-dev', // Usually your GitHub org/user name.
+  projectName: 'guide-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
