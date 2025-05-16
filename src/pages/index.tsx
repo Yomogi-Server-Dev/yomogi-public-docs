5f7c7ba59dfedf3d61dca025b03e3be0622f0cbf
相田 優希 <51500566+aida0710@users.noreply.github.com>
2025-05-16 16:03:21 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
+import {useEffect} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
     const {siteConfig} = useDocusaurusContext();
+
+    useEffect(() => {
+        window.location.href = '/docs/intro';
+    }, []);
 
     return (
         <Layout
             title={`${siteConfig.title}へようこそ！`}
             description="Yomogi Server Guide">
-            <Head>
-                <meta httpEquiv="refresh" content="0;url=/docs/intro" />
-            </Head>
-            <div style={{padding: '2rem', textAlign: 'center'}}>
-                リダイレクト中...
-            </div>
         </Layout>
     );
 }
